2f3edc093d0c715a07835c396c559ae7fe5b2764
Imran Abdurakhmanov <petrow318@gmail.com>
2021-12-11 15:59:29 +0300

    рефакторинг

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./redux/configureStore";
 import "./styles.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
